a5d53142e670ff73a015dd84078fdbff804abf5a
zhyun <like4m@naver.com>
2023-11-06 23:36:29 +0900

    수정

--- src/investmentApi.py
+++ src/investmentApi.py
@@ -5,74 +5,61 @@ with open('./config/stock_code.yaml', encoding='UTF-8') as f:
     _code = yaml.load(f, Loader=yaml.FullLoader)
 
 
-def initInvestement():
+def init_investment():
     kis.ACCESS_TOKEN = kis.get_access_token()
-    #kis.ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6ImExNDEyMWJkLWRjZmMtNDYyNi1hNWViLThiNTNmZWI5ZmUwYiIsImlzcyI6InVub2d3IiwiZXhwIjoxNjk5MjcyNjk2LCJpYXQiOjE2OTkxODYyOTYsImp0aSI6IlBTc1lIdk9yMTBUSkFnbW9uOTN6TWhrUk84ZTZBcHl6YjZubCJ9.DrGvvSPP_pOrYZbqBDfBLJQoA0E2BX1JgCdquHxFblWU3rA2ZbpzePNjV_C15fS8HN7K66ZfEu7qDNgfpDlSTQ"
+    # kis.ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6ImExNDEyMWJkLWRjZmMtNDYyNi1hNWViLThiNTNmZWI5ZmUwYiIsImlzcyI6InVub2d3IiwiZXhwIjoxNjk5MjcyNjk2LCJpYXQiOjE2OTkxODYyOTYsImp0aSI6IlBTc1lIdk9yMTBUSkFnbW9uOTN6TWhrUk84ZTZBcHl6YjZubCJ9.DrGvvSPP_pOrYZbqBDfBLJQoA0E2BX1JgCdquHxFblWU3rA2ZbpzePNjV_C15fS8HN7K66ZfEu7qDNgfpDlSTQ"
     print(kis.ACCESS_TOKEN)
 
 
 # 보유 현금 조회
-def getBalanceCash():
-    res = kis.get_balance()
-    print(res.text)
-    cash = res.json()['output']['ord_psbl_cash']
+def get_balance_cash():
+    try:
+        res = kis.get_balance()
+        print(res.text)
+        cash = res.json()['output']['ord_psbl_cash']
+    except Exception as e:
+        logger.error(f"[보유 현금 조회 오류 발생]{e}")
     return int(cash)
 
 
-# 보유 주식 조회
-def getBalanceStock():
-    print("보유 주식 조회")
-    res = kis.get_stock_balance()
-    stock_list = res.json()['output1']
-    evaluation = res.json()['output2']
-    stock_dict = {}
-    for stock in stock_list:
-        if int(stock['hldg_qty']) > 0:
-            stock_dict[stock['pdno']] = stock['hldg_qty']
-    #         send_message(f"{stock['prdt_name']}({stock['pdno']}): {stock['hldg_qty']}주")
-    #         time.sleep(0.1)
-    # send_message(f"주식 평가 금액: {evaluation[0]['scts_evlu_amt']}원")
-    # time.sleep(0.1)
-    # send_message(f"평가 손익 합계: {evaluation[0]['evlu_pfls_smtl_amt']}원")
-    # time.sleep(0.1)
-    # send_message(f"총 평가 금액: {evaluation[0]['tot_evlu_amt']}원")
-    # time.sleep(0.1)
-    print(stock_list)
-    return stock_dict
+# 주식 전체 매도
+# def sell_all_stocks(stock_dict, wish_stock_dict):
+#     failCodes = []
+#     res = {}
+#     res["rtnCd"] = "S"
+#     for code, qty in stock_dict.items():
+#         if code in wish_stock_dict.keys():  # 매수 희망 종목 리스트에 포함된 주식만 해당 프로그램에서 다룬다.
+#             try:
+#                 res = kis.sell(code, qty)
+#                 if res.json()['rt_cd'] != '0':
+#                     logger.error("[매도 실패]" + res.json()['msg'])
+#                     failCodes.append(code)
+#                     res["rtnCd"] = "F"
+#             except Exception as e:
+#                 logger.error(f"[매도 오류 발생]{e}")
+#     res["failCodes"] = failCodes
+# 
+#     return res
 
 
-# 주식 매도
-def sellAllStocks(stock_dict, wish_stock_dict):
-    failCodes = []
-    res = {}
-    res["rtnCd"] = "S"
-    for code, qty in stock_dict.items():
-        if code in wish_stock_dict.keys():  # 매수 희망 종목 리스트에 포함된 주식만 해당 프로그램에서 다룬다.
-            try:
-                res = kis.sell(code, qty)
-                if res.json()['rt_cd'] != '0':
-                    logger.error("[매도 실패]" + res.json()['msg'])
-                    failCodes.append(code)
-                    res["rtnCd"] = "F"
-            except Exception as e:
-                logger.error(f"[매도 오류 발생]{e}")
-    res["failCodes"] = failCodes
-
+def get_stock_price_daily_info(code):
+    try:
+        res = kis.get_stock_price(code)
+    except Exception as e:
+        logger.error(f"[주식 일별 정보 조회 오류 발생]{e}")
     return res
 
 
-def getStockPriceDailyInfo(code):
-    res = kis.get_stock_price(code)
+def get_stock_cur_price(code):
+    try:
+        res = kis.get_current_price(code)
+        time.sleep(1)
+    except Exception as e:
+        logger.error(f"[주식 현재가 조회 오류 발생]{e}")
     return res
 
 
-def getStockCurPrice(code):
-    res = kis.get_current_price(code)
-    time.sleep(1)
-    return res
-
-
-def buyStock(code, buy_qty):
+def buy_stock(code, buy_qty):
     try:
         res = kis.buy(code, buy_qty)
         if res.json()['rt_cd'] == '0':
@@ -81,7 +68,8 @@ def buyStock(code, buy_qty):
         logger.error(f"[주식매매 오류 발생]{e}")
     return False
 
-def sellStrock(code, buy_qty):
+
+def sell_stock(code, buy_qty):
     try:
         res = kis.sell(code, buy_qty)
         if res.json()['rt_cd'] == '0':
@@ -92,7 +80,7 @@ def sellStrock(code, buy_qty):
     return False
 
 
-def initTrgtStockList(symbol_list):
+def init_trgt_stock_list(symbol_list):
     rtnRes = {}
     """ 
     0: 오늘 시가
@@ -102,39 +90,42 @@ def initTrgtStockList(symbol_list):
     4: 매수 목표가
     5: 매매 목표가    
     """
-    for code in symbol_list:
-        logger.info(f"{_code[code]}[{code}]")
-        arr = []
-        res = getStockPriceDailyInfo(code)
-
-        stck_oprc = int(res.json()['output'][0]['stck_oprc'])  # 오늘 시가
-        stck_hgpr = int(res.json()['output'][1]['stck_hgpr'])  # 전일 고가
-        stck_lwpr = int(res.json()['output'][1]['stck_lwpr'])  # 전일 저가
-        stck_clpr = int(res.json()['output'][1]['stck_clpr'])  # 전일 종가
-
-        # 0 : 종가의 2% 상승 시 목표가
-        target_price = int(get_target_price(0, stck_oprc, stck_hgpr, stck_lwpr, stck_clpr))
-        sell_target_price = int(target_price + target_price * SELL_PER)
-
-        arr.append(stck_oprc)  # 오늘 시가
-        arr.append(stck_hgpr)  # 전일 고가
-        arr.append(stck_lwpr)  # 전일 저가
-        arr.append(stck_clpr)  # 전일 종가
-        arr.append(target_price)  # 매수 목표가
-        arr.append(sell_target_price)  # 매매 목표가
-
-        print(f"오늘 시가: {stck_oprc}")
-        print(f"전일 고가: {stck_hgpr}")
-        print(f"전일 저가: {stck_lwpr}")
-        print(f"전일 종가: {stck_clpr}")
-        print(f"매수 목표가:    {target_price}")
-        print(f"매도 목표가:    {sell_target_price}")
-
-        msg = _code[code] + "[" + code + "]" + "\n" + "오늘 시가: " + str(stck_lwpr) + "\n" + "전일 종가: " \
-              + str(stck_clpr) + "\n" + "매수목표가: " + str(target_price) + "\n" + "매도목표가: " + str(
-            sell_target_price)
-        send_message(msg)
-        rtnRes[code] = arr
+    try:
+        for code in symbol_list:
+            logger.info(f"{_code[code]}[{code}]")
+            arr = []
+            res = get_stock_price_daily_info(code)
+    
+            stck_oprc = int(res.json()['output'][0]['stck_oprc'])  # 오늘 시가
+            stck_hgpr = int(res.json()['output'][1]['stck_hgpr'])  # 전일 고가
+            stck_lwpr = int(res.json()['output'][1]['stck_lwpr'])  # 전일 저가
+            stck_clpr = int(res.json()['output'][1]['stck_clpr'])  # 전일 종가
+    
+            # 0 : 종가의 2% 상승 시 목표가
+            target_price = int(get_target_price(0, stck_oprc, stck_hgpr, stck_lwpr, stck_clpr))
+            sell_target_price = int(target_price + target_price * SELL_PER)
+    
+            arr.append(stck_oprc)  # 오늘 시가
+            arr.append(stck_hgpr)  # 전일 고가
+            arr.append(stck_lwpr)  # 전일 저가
+            arr.append(stck_clpr)  # 전일 종가
+            arr.append(target_price)  # 매수 목표가
+            arr.append(sell_target_price)  # 매매 목표가
+    
+            print(f"오늘 시가: {stck_oprc}")
+            print(f"전일 고가: {stck_hgpr}")
+            print(f"전일 저가: {stck_lwpr}")
+            print(f"전일 종가: {stck_clpr}")
+            print(f"매수 목표가:    {target_price}")
+            print(f"매도 목표가:    {sell_target_price}")
+    
+            msg = _code[code] + "[" + code + "]" + "\n" + "오늘 시가: " + str(stck_lwpr) + "\n" + "전일 종가: " \
+                  + str(stck_clpr) + "\n" + "매수목표가: " + str(target_price) + "\n" + "매도목표가: " + str(
+                sell_target_price)
+            send_message(msg)
+            rtnRes[code] = arr
+    except Exception as e:
+        logger.error(f"[목표 주식 초기화 오류 발생]{e}")
     return rtnRes
 
 
@@ -161,6 +152,7 @@ def report_cur_stock_info(dict_bought_list, wish_stock_dict):
     logger.info("=====reportCurStockInfo END =====")
     return True
 
+
 def get_my_stock_cur_amt(wish_stock_dict):
     try:
         rtnRes = {}
@@ -171,21 +163,9 @@ def get_my_stock_cur_amt(wish_stock_dict):
 
         for stock in stock_list:
             if stock['pdno'] in wish_stock_dict.keys():
-                print(f"*{stock['prdt_name']}\n매입[{stock['pchs_amt']}]/[{stock['hldg_qty']}] / 현재가[{stock['prpr']}] / 평가손익금액[{stock['evlu_pfls_amt']}]\n")
+                print(
+                    f"*{stock['prdt_name']}\n매입[{stock['pchs_amt']}]/[{stock['hldg_qty']}] / 현재가[{stock['prpr']}] / 평가손익금액[{stock['evlu_pfls_amt']}]\n")
                 rtnRes[stock['pdno']] = stock['prpr']
     except Exception as e:
         logger.error(f"[보유 주식 현재가 조회 오류 발생]{e}")
     return rtnRes
-
-def getRealProfit():
-    res = kis.get_Real_Profit()
-    logger.info(res)
-    write_report(res)
-    return True
-
-
-def TEST():
-    ttime = datetime.datetime.now()
-    global REPORT
-    REPORT = "nononono"
-    print(f"{ttime} {REPORT}")
--- src/main.py
+++ src/main.py
@@ -10,38 +10,39 @@ with open('./config/stock_code.yaml', encoding='UTF-8') as f:
 
 # 삼성전자: 005930 카카오: 035720 하이닉스: 000660 세틀뱅크: 234340 현대차: 005380 나이스정보통신: 036800 LG전자: 066570 LG유플러스: 032640
 # 한화: 000880 롯데정보통신: 286940 CJ CGV: 079160 롯데지주: 004990 삼천리: 004690 대한항공: 003490 네이버: 035420 두산로보티스: 454910
-symbol_list = ["234340", "000660", "035720", "036800", "066570", "032640", "000880", "005380"
-               "286940", "079160", "069960", "004990", "004990", "004690", "003490", "035420",
-               "454910"]  # 매수 희망 종목 리스트
+symbol_list = ["234340", "000660", "035720", "036800", "066570", "032640", "000880", "005380", "286940", "079160", "069960",
+               "004990", "004990", "004690", "003490", "035420", "454910"]  # 매수 희망 종목 리스트
 
 logger.info("======================Start the program. Let's be rich======================")
 # KIS 초기화
-initInvestement()
+init_investment()
 
 # 보유 현금 조회
-total_cash = getBalanceCash()
+total_cash = get_balance_cash()
 
 wish_stock_dict = {}  # 매수 희망 종목 정보
 dict_bought_list = {}  # 매수 완료 정보
 
-wish_stock_dict = initTrgtStockList(symbol_list)
+wish_stock_dict = init_trgt_stock_list(symbol_list)
 report_cur_stock_info(dict_bought_list, wish_stock_dict)
 
 isReportTime = False
 
-def setReportTime():
+
+def set_report_time():
     global isReportTime
     isReportTime = True
 
+
 # 스케쥴러 초기화
 # schedule.every(1).hour.do(reportCurStockInfo, dict_bought_list, wish_stock_dict) # 매시각 보고
-schedule.every(30).minutes.do(setReportTime)  # 30분마다 보고
+schedule.every(30).minutes.do(set_report_time)  # 30분마다 보고
 # schedule.every(5).seconds.do(setReportTime)  # 30분마다 보고
 # schedule.run_pending()  # 정시에 현재 보유 주식 정보를 보고받고자 스케쥴러 실행
 while True:
     try:
 
-        if isReportTime:    # 스케쥴러로 정해진 시간에 중간 보유 주식 보고
+        if isReportTime:  # 스케쥴러로 정해진 시간에 중간 보유 주식 보고
             report_cur_stock_info(dict_bought_list, wish_stock_dict)
             isReportTime = False
 
@@ -59,19 +60,6 @@ while True:
         #     break
         # if t_9 <= t_now <= t_start:
 
-        # 잔여 수량 매도
-        # stock_dict = getBalanceStock()  # 보유 주식 조회
-        # res = sellAllStocks(stock_dict, symbol_list)
-        # if res["rtnCd"] != "S":
-        #     msg = ""
-        #     for code in res["failCodes"]:
-        #         msg += f"{_code[code]}/"
-        #     send_message(f"[매도 실패]: {msg}")
-
-        # 대상 종목 초기화
-        # wish_stock_dict = initTrgtStockList(symbol_list)
-
-        # send_message("보유현금: " + str(total_cash))
         if t_start <= t_now:
             schedule.run_pending()  # 정시에 현재 보유 주식 정보를 보고받고자 스케쥴러 실행
         if t_start <= t_now <= t_buy:  # 09:00 ~ 10:30 까지만 매수
@@ -88,12 +76,12 @@ while True:
                     continue
 
                 # 종목 현재가 조회
-                current_price = getStockCurPrice(code)
+                current_price = get_stock_cur_price(code)
                 logger.info(f"{_code[code]} 현재가 [{current_price}] / 매수목표가 [{arrTmp[4]}]")
 
                 buy_qty = 0
                 # 목표가보다 현재가가 높은 경우 매수 진행
-                if arrTmp[4] <= current_price: # <= int(arrTmp[5] * 1.05):        # 급등 항목은 제외 될 수 있도록
+                if arrTmp[4] <= current_price:  # <= int(arrTmp[5] * 1.05):        # 급등 항목은 제외 될 수 있도록
                     # target_buy_count = current_price // standard_price_stock      # 1주당 금액 기준으로 매수 수량 선택
                     target_buy_count = STANDARD_PRICE_STOCK // current_price
                     if target_buy_count == 0:
@@ -102,10 +90,10 @@ while True:
                         buy_qty = target_buy_count
                     try:
                         send_message(f"{_code[code]} 목표가 달성({arrTmp[4]} <= {current_price}) 매수를 시도합니다.")
-                        if buyStock(code, buy_qty):
+                        if buy_stock(code, buy_qty):
                             dict_bought_list[code] = buy_qty
                             tmp_sell_target_price = wish_stock_dict[code][5]
-                            wish_stock_dict[code][5] = int(current_price * (1+SELL_PER))  # 매수 금액으로 매도 목표 금액 재설정 (2%)
+                            wish_stock_dict[code][5] = int(current_price * (1 + SELL_PER))  # 매수 금액으로 매도 목표 금액 재설정 (2%)
                             send_message(f"[매수 성공]: {_code[code]}({buy_qty})")
                             write_report(f"[매수 성공]: {_code[code]}({buy_qty})")
                             # f"매도 목표가 변경 {tmp_sell_target_price} -> {wish_stock_dict[code][5]}")
@@ -114,15 +102,15 @@ while True:
                             send_message(f"[매수 실패]: {_code[code]}({buy_qty})")
                     except Exception as e:
                         logger.error(f"[매수 오류 발생]{e}")
-        if t_buy <= t_now <= t_sell:    # 11:00 ~ 15:20 까지 매도 진행
+        if t_buy <= t_now <= t_sell:  # 11:00 ~ 15:20 까지 매도 진행
             # 매수한 종목이 금일 매수금액 대비 2% 이상이면 욕심부리지 말고 팔자. 미반영
             print("=====매도목표가 달성 시 매도 진행")
             if isReportTime:
                 time.sleep(2)
-            dict_cur_amt = get_my_stock_cur_amt(wish_stock_dict)    # 매수 종목 현재가 가져오기
+            dict_cur_amt = get_my_stock_cur_amt(wish_stock_dict)  # 매수 종목 현재가 가져오기
             time.sleep(1)
             if len(dict_cur_amt) == 0:
-                continue    # 보유 주식 없는 경우 PASS
+                continue  # 보유 주식 없는 경우 PASS
 
             for code in list(wish_stock_dict.keys()):
                 if code in dict_bought_list:
@@ -134,14 +122,14 @@ while True:
                         send_message(f"{_code[code]} 목표가 달성({arrTmp[4]} <= {current_price}) 매도를 시도합니다.")
                         if code in symbol_list:
                             try:
-                                if sellStrock(code, dict_bought_list[code]):
+                                if sell_stock(code, dict_bought_list[code]):
                                     send_message(f"[매도 성공]: {_code[code]}({dict_bought_list[code]})")
                                     del dict_bought_list[code]
                                     del wish_stock_dict[code]
                             except Exception as e:
                                 logger.error(f"[매도 오류 발생]{e}")
                 # time.sleep(1)
-        if t_sell < t_now :  # PM 03:20 ~ PM 03:25 : 일괄 매도
+        if t_sell < t_now:  # PM 03:20 ~ PM 03:25 : 일괄 매도
             if len(dict_bought_list) > 0:
                 # 잔여 수량 매도
                 for code in list(wish_stock_dict.keys()):
@@ -149,16 +137,16 @@ while True:
                         arrTmp = wish_stock_dict[code]
                         if code in symbol_list:
                             try:
-                                if sellStrock(code, dict_bought_list[code]):
+                                if sell_stock(code, dict_bought_list[code]):
                                     send_message(f"[매도 성공]: {_code[code]}({dict_bought_list[code]})")
                                     del dict_bought_list[code]
                                     del wish_stock_dict[code]
                                     time.sleep(0.5)
                             except Exception as e:
                                 logger.error(f"[매도 오류 발생]{e}")
-        if t_exit < t_now :  # PM 03:20 ~ :프로그램 종료
+        if t_exit < t_now:  # PM 03:20 ~ :프로그램 종료
             report_cur_stock_info(dict_bought_list, wish_stock_dict)
-            diff_cash = getBalanceCash() - total_cash
+            diff_cash = get_balance_cash() - total_cash
             send_message(f"금일 수익: {diff_cash}")
             write_report(f"금일 수익: {diff_cash}")
             break
